b66201833cbc403ecc6d01e4b8d56bd9517952e1
BDex0tED <bemanaff@gmail.com>
2026-01-25 22:22:46 +0600

    initial commit

---
+++ src/main/java/com/ivyro/pdd/TrafficRulesApplication.java
@@ -0,0 +1,13 @@
+package com.ivyro.pdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficRulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficRulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivyro/pdd/TrafficRulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivyro.pdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficRulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
